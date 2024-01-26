86c2b5b6387fdd90bd91ceb2721a6d618e5e1c1a
Nguyen Duy Minh Thong <nguyenduyminhthong@gmail.com>
2024-01-27 05:10:29 +0700

    Add pip install step to zip_lambda_folders.sh script

--- cmd/zip_lambda_folders.sh
+++ cmd/zip_lambda_folders.sh
@@ -3,6 +3,8 @@ cd ./src/lambda
 for folder in */; do
     folder_name="${folder%/}"
 
+    pip install -t "${folder}" -r ../../requirements.txt
+
     cd "$folder"
     zip -9 -r ../${folder_name}.zip .
     cd ..
